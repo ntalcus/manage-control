b6b562df258d3a17d85e1b74d9c91a7e55f1c4ab
ntalcus <alcus.noah@gmail.com>
2018-09-01 15:42:20 -0700

    basic application

---
+++ application.php
@@ -0,0 +1,25 @@
+<?php
+
+class application {
+    var $startTime;
+    var $version;
+    var $database;
+
+    public function setStartTime() {
+        if !isset($startTime) {
+            $startTime = time();
+        }
+        return $startTime;
+    }
+
+    public function initializeDatabase() {
+        # Grab the database, initialize if not present
+    }
+
+    public function
+
+
+}
+
+
+?>
